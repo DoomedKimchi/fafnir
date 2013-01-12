1aacec3f74d30ffd27211954871b0bb8e2a830cd
Freddy Kellison-Linn <fred.kl@me.com>
2013-01-12 13:30:08 -0800

    Initial commit

---
+++ Fafnir.cpp
@@ -0,0 +1,25 @@
+#include "WPILib.h"
+
+class Fafnir : public IterativeRobot {
+
+
+public:
+	Fafnir() {
+		
+	}
+
+	void AutonomousInit() {
+		
+	}
+
+	void TeleopInit() {
+
+	}
+	
+	void Test() {
+
+	}
+};
+
+START_ROBOT_CLASS(Fafnir);
+
