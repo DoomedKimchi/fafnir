f9cb9e00ec4a5d531bc6d641e28c5652234f6724
Sauyon <git@sauyon.com>
2013-01-14 17:43:34 -0800

    created method stubs in DriveTrain and created Constants.h

--- DriveTrain.h
+++ DriveTrain.h
@@ -1,18 +1,15 @@
 #ifndef DRIVE_TRAIN_H
 #define DRIVE_TRAIN_H
 
-/*
- * Drivetrain class
- * It's the drive train, what do you think it does?
- * Are you seriously still reading this?
- */
+#include "Constants.h"
 
 class DriveTrain {
-private:
-  
+ private:
+  enum ShifterMode { LOW_GEAR, HIGH_GEAR, AUTO };
 
-public:
-
+ public:
+  boolean setMode(ShifterMode mode);
+  boolean update();
 };
 
-#endif /* DRIVE_TRAIN_H *?
+#endif /* DRIVE_TRAIN_H */
