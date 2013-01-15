b26459ee6b34c4b0f30fb0f6d06f0435f4e6d933
Nathan Kau <nathankau@gmail.com>
2013-01-15 15:46:08 -0800

    add stub to drvierstation

--- DriverStation.h
+++ DriverStation.h
@@ -1,11 +1,18 @@
 #ifndef DRIVER_STATION_H
 #define DRIVER_STATION_H
 
+#include "WPILib.h"
+
 class DriverStation {
 private:
-
+	Joystick leftStick;
+	Joystick rightStick;
+	// buttons and things
 public:
   void update();
+  int getAutoType(); // we can set different autonomous starting places or procedures
+  int getStickX(Joystick stick); // pass in which joystick to get
+  int getStickY(Joystick stick); // pass in which joystick to get
 };
 
 #endif /* DRIVER_STATION_H */
