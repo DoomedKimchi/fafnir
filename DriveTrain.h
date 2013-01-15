d41c4afbf3327c6f7ca0eb610648c58a6f795619
chinstorff <christopherhinstorff@me.com>
2013-01-14 17:10:54 -0800

    created stubs for DriveTrain

--- DriveTrain.h
+++ DriveTrain.h
@@ -9,10 +9,26 @@
 
 class DriveTrain {
 private:
-  
+  bool low_gear;
+  bool automatic;
+  Encoder enc_left_front;
+  Encoder enc_left_back;
+  Encoder enc_right_front;
+  Encoder enc_right_back;
 
 public:
+  void drive(int); //straight forward or straight backward
+  void drive(int,int); //control speeds of left/right
+  void drive(int,int,int,int) //control each motor
+  void rotate(int, int); //parameters: rotate angle, rotate speed
 
+  bool getGear();
+  void setGear(bool);
+
+  void startAuto();
+  void endAuto();
+  void isAuto();
+  void periodicAuto();
 };
 
 #endif /* DRIVE_TRAIN_H *?
