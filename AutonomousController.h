4924f85dec0ca75dcf1789f5eb57fb0618b5636d
Sauyon Lee <sauyon@phobos.(none)>
2013-01-16 17:43:16 -0800

    added an ivar for Robot and a couple methods

--- AutonomousController.h
+++ AutonomousController.h
@@ -1,11 +1,15 @@
 #ifndef AUTONOMOUS_CONTROLLER_H
 #define AUTONOMOUS_CONTROLLER_H
 
+#include "Robot.h"
+
 class AutonomousController {
-private:
-  
-public:
-  
+ private:
+  Robot *fafnir;
+ public:
+  AutonomousController(Robot *fafnir);
+  boolean fire();
+  virtual ~AutonomousController();
 };
 
 #endif /* AUTONOMOUS_CONTROLLER_H */
