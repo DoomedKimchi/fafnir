b4023cdd8d8ffbbadeaea430a593561883784969
Nathan Kau <nathankau@gmail.com>
2013-01-17 16:59:51 -0800

    removed drive methods

--- Robot.h
+++ Robot.h
@@ -15,11 +15,10 @@ private:
    AutonomousController autonomousController;
    Accumulator accumulator;
    AxisCamera &camera;
+
+
 public:
    Robot();
-   void drive(float speed); //drive forward at speed
-   void drive(float speed, float speed); // left, right motor set speeds
-   void drive(float speed, float curvature); // drive at speed and curvature
    void accumulate(); // take care of sensing?
    void shoot(); // just shoot, no aiming
    void aim(float yaw, float elevation); // angle to aim
