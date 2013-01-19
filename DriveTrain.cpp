f59cd2fb91296dd1232b8969a4357c96be1e1258
Freddy Kellison-Linn <fred.kl@me.com>
2013-01-19 13:21:52 -0800

    reformat DriveTrain.cpp

--- DriveTrain.cpp
+++ DriveTrain.cpp
@@ -4,38 +4,38 @@
  * Are you seriously still reading this?
  */
 
-DriveTrain:: periodicAuto() {
+DriveTrain::periodicAuto() {
 
 }
 
-DriveTrain:: driveSpd(double d) {
+DriveTrain::driveSpd(double d) {
 
 }
 
-DriveTrain:: driveDist(double d) {
+DriveTrain::driveDist(double d) {
 
 }
 
-DriveTrain:: driveTo(double d1, double d2) {
+DriveTrain::driveTo(double d1, double d2) {
 
 }
 
-DriveTrain:: rotateSpd(double d) {
+DriveTrain::rotateSpd(double d) {
 
 }
 
-DriveTrain:: rotateDist(double d) {
+DriveTrain::rotateDist(double d) {
 
 }
 
-DriveTrain:: getGear() {
+DriveTrain::getGear() {
 
 }
 
-DriveTrain:: setGear(ShifterMode s) {
+DriveTrain::setGear(ShifterMode s) {
 
 }
 
-DriveTrain:: update() {
+DriveTrain::update() {
 
 }
