85850fd67ef850f2f06e3824caa96892358fd3b4
Christopher Hinstorff <christopherhinstorff@me.com>
2013-01-19 17:07:02 -0800

    change all ports to 0

--- Constants.h
+++ Constants.h
@@ -2,25 +2,25 @@
 #define CONSTANTS_H
 
 /* Solenoid port numbering */
-#define PORT_DRIVE_SOL_1 1
-#define PORT_DRIVE_SOL_2 2
+#define PORT_DRIVE_SOL_1 0
+#define PORT_DRIVE_SOL_2 0
 
 /* Encoder port numbering */
-#define PORT_DRIVE_ENC_1 3
-#define PORT_DRIVE_ENC_2 4
-#define PORT_DRIVE_ENC_3 5
-#define PORT_DRIVE_ENC_4 6
+#define PORT_DRIVE_ENC_1 0
+#define PORT_DRIVE_ENC_2 0
+#define PORT_DRIVE_ENC_3 0
+#define PORT_DRIVE_ENC_4 0
 
 /* DriveTrain Victor port numbering */
-#define PORT_DRIVE_VIC_1 7
-#define PORT_DRIVE_VIC_2 8
-#define PORT_DRIVE_VIC_3 9
-#define PORT_DRIVE_VIC_4 10
+#define PORT_DRIVE_VIC_1 0
+#define PORT_DRIVE_VIC_2 0
+#define PORT_DRIVE_VIC_3 0
+#define PORT_DRIVE_VIC_4 0
 
 /* Joystick port numbering */
-#define PORT_JOYSTICK_SPEED 11
-#define PORT_JOYSTICK_TURN 12
-#define PORT_JOYSTICK_OPERATOR 13
+#define PORT_JOYSTICK_SPEED 0
+#define PORT_JOYSTICK_TURN 0
+#define PORT_JOYSTICK_OPERATOR 0
 
 /* Solenoid definitions */
 #define SOL_HIGH false
