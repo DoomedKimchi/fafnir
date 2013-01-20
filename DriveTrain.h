0acb5d91b0025d17e90ded40b672b5d5e0db71ca
Freddy Kellison-Linn <fred.kl@me.com>
2013-01-20 12:31:54 -0800

    add gyro to DriveTrain

--- DriveTrain.h
+++ DriveTrain.h
@@ -2,7 +2,7 @@
 #define DRIVE_TRAIN_H
 
 #include "Constants.h"
-#include "WPILabs.h"
+#include "WPILib.h"
 
 class DriveTrain {
 private:
@@ -21,12 +21,18 @@ private:
   Victor rightFrontVic;
   Victor rightBackVic;
 
+  Gyro gyro;
+
   int speed;
   int rotateSpeed;
 
   void engageHigh();
   void engageLow();
   void engageAuto();
+
+  float targetSpeed;
+  float targetDist;
+  float targetAngle;
 
 public:
   bool driveSpd(double);
