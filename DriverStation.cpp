9107d2961e8aa6c089c5c0db79c5d06b36dbfb98
Christopher Hinstorff <christopherhinstorff@me.com>
2013-01-19 16:55:32 -0800

    add empty methods

--- DriverStation.cpp
+++ DriverStation.cpp
@@ -1,1 +1,22 @@
 
+DriverStation::DriverStation() {
+  speedStick(PORT_JOYSTICK_SPEED);
+  turnStick(PORT_JOYSTICK_TURN);
+  operatorStick(PORT_JOYSTICK_OPERATOR);
+}
+
+int DriverStation::getAutoType() {
+
+}
+
+int DriverStation::getStickX() {
+
+}
+
+int DriverStation::getStickY() {
+
+}
+
+void DriverStation::update() {
+  
+}
