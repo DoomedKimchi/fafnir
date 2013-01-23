3825517af1a6f87db872a8a9b2af2af5c88fbba6
Sauyon <git@sauyon.com>
2013-01-22 17:09:44 -0800

    Yay for AutonomousController!

--- search/all_61.js
+++ search/all_61.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['accumulator',['Accumulator',['../classAccumulator.html',1,'Accumulator'],['../classAccumulator.html#a19845ea4f3d7bee830437adcdf197e3e',1,'Accumulator::Accumulator()']]],
   ['autoaccumulate',['autoAccumulate',['../classAccumulator.html#a89d4cff8afe6b2535e7b21f0e4074ac5',1,'Accumulator']]],
-  ['autonomouscontroller',['AutonomousController',['../classAutonomousController.html',1,'']]]
+  ['autonomouscontroller',['AutonomousController',['../classAutonomousController.html',1,'AutonomousController'],['../classAutonomousController.html#a4cde33d2ffd352a19026cd28c4e3c0b2',1,'AutonomousController::AutonomousController()']]]
 ];
