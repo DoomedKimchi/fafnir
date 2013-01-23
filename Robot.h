b18fcf9e3170bd4e221a40901d4ac5cc298446a7
Jeremy Samos <battlefront123@gmail.com>
2013-01-23 00:41:02 +0000

    fix up robot

---
+++ Robot.h
@@ -0,0 +1,30 @@
+#ifndef ROBOT_H
+#define ROBOT_H
+
+#include "WPILib.h"
+#include "Constants.h"
+#include "Accumulator.h"
+#include "AutonomousController.h"
+#include "DriverStation.h"
+#include "Shooter.h"
+
+class Robot {
+ private:
+  DriveTrain driveTrain;
+  Accumulator accumulator;
+  Shooter shooter;
+
+  AxisCamera &camera;
+  Compressor compressor;
+
+  bool isAimed = true;
+  
+ public:
+  Robot();
+  void accumulate(); // take care of sensing?
+  void shoot(); // just shoot, no aiming
+  void aim(float yaw, float elevation); // angle to aim
+  void aim(); // aim using the camera
+  void climb();
+};
+#endif /* ROBOT_H */
