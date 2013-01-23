f69e6b983426ec11820a7d811e4878d13cb3223b
Sauyon <git@sauyon.com>
2013-01-22 17:07:50 -0800

    Added doxy comments

--- AutonomousController.h
+++ AutonomousController.h
@@ -3,12 +3,26 @@
 
 #include "Robot.h"
 
+/*! \brief Controller for the autonomous period
+ *
+ * AutonomousController controls the robot during the autonomous period of the competition
+ * Currently, it only fires frisbees at targets */
+
 class AutonomousController {
  private:
+  /*! A pointer to the Robot fafnir */
   Robot *fafnir;
+
+  /*! \brief Tells the robot to fire
+   * \return Whether firing was successful */
   bool fire();
+
  public:
+  /*! \brief Constructs an autonomous controller with a pointer to fafnir
+   * \param *fafnir A pointer to the Robot */
   AutonomousController(Robot *fafnir);
+
+  /*! \brief Destructor */
   virtual ~AutonomousController();
 };
 
