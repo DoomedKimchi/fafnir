45569aa84f66b55f35a69c2563b34537d971e2a5
Sauyon <git@sauyon.com>
2013-01-24 20:31:55 -0800

    Commented!

--- Robot.h
+++ Robot.h
@@ -8,23 +8,43 @@
 #include "DriverStation.h"
 #include "Shooter.h"
 
+/*! /brief controls all the robot's functions
+ *
+ * This class provides a top-level interface for controlling the robot's functions
+ * It also does the aiming for the robot */
+
 class Robot {
  private:
+  /*! The drive train of the robot */
   DriveTrain driveTrain;
+  /*! The accumulator of the robot */
   Accumulator accumulator;
+  /*! The shooter of the robot */
   Shooter shooter;
 
+  /*! The robot's camera */
   AxisCamera &camera;
+  /*! The robot's compressor */
   Compressor compressor;
 
-  bool isAimed = true;
+  /*! Whether the robot is aimed at its target */
+  bool isAimed;
   
  public:
+  /*! \brief Default constructor */
   Robot();
-  void accumulate(); // take care of sensing?
-  void shoot(); // just shoot, no aiming
-  void aim(float yaw, float elevation); // angle to aim
-  void aim(); // aim using the camera
+  /*! \brief Accumulates frisbees
+   * \internal may take care of sensing*/
+  void accumulate();
+  /*! \brief Fires a frisbee in the current direction of the robot */
+  void shoot();
+  /*! \brief Aims at a certain point
+   * \param yaw The yaw to aim at
+   * \param pitch The pitch to aim at */
+  void aim(float yaw, float pitch);
+  /*! \brief Aims at a target using the camera */
+  void aim();
+  /*! \brief Climbs the pyramid */
   void climb();
 };
 #endif /* ROBOT_H */
