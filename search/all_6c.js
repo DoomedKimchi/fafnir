4e5e08e763d1f65f5d70ca0a4337426cbc403c0f
Sauyon <git@sauyon.com>
2013-01-24 19:39:12 -0800

    regen'd

--- search/all_6c.js
+++ search/all_6c.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['linearvictor',['LinearVictor',['../classLinearVictor.html',1,'']]]
+  ['linearvictor',['LinearVictor',['../classLinearVictor.html',1,'LinearVictor'],['../classLinearVictor.html#a6f75ab8229f99f04ff52159bbf08547e',1,'LinearVictor::LinearVictor()']]]
 ];
--- search/all_73.js
+++ search/all_73.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['set',['set',['../classLinearVictor.html#a01b6b664706e430aa20322c41b2ab105',1,'LinearVictor']]],
   ['setmode',['setMode',['../classDriveTrain.html#a2f633207590a06ef4df640791945ae58',1,'DriveTrain']]],
   ['shooter',['Shooter',['../classShooter.html',1,'']]]
 ];
