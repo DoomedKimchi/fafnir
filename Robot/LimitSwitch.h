a8769e1729438163d11e4c4d6e7ac5d336a4f459
Freddy Kellison-Linn <fred.kl@me.com>
2013-02-11 16:09:24 -0800

    add LimitSwitch

---
+++ Robot/LimitSwitch.h
@@ -0,0 +1,10 @@
+#ifndef LIMIT_SWITCH_H
+#define LIMIT_SWITCH_H
+#include "WPILib.h"
+class LimitSwitch : public DigitalInput {
+public:
+    LimitSwitch(int CHANNEL);
+    LimitSwitch(int MODULE, int CHANNEL);
+    bool IsPressed();
+};
+#endif /* LIMIT_SWITCH_H */
