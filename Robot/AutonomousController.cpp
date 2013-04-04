fd3fbc6c8673039c49005fd69d02f4e449556522
chinstorff <christopherhinstorff@me.com>
2013-04-04 14:41:38 -0700

    It's %lu not %ul obviously.

--- Robot/AutonomousController.cpp
+++ Robot/AutonomousController.cpp
@@ -68,7 +68,7 @@ void AutonomousController::update() {
   //Wait(1);
   if (targetReached)
     dump(); */
-  printf("%ul", timer.Get());
+  printf("%lu", timer.Get());
   if (timer.Get() < 1000000) {
      robot->setSpeed(-0.8);
   }
