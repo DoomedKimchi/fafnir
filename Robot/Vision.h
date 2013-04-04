f74b3740f44e8eeb5b87f068beb3338caa578be4
William Meng <wlmeng11@gmail.com>
2013-04-03 23:21:19 -0700

    Framework for vision processing

---
+++ Robot/Vision.h
@@ -0,0 +1,17 @@
+#ifndef VISION_H
+#define VISION_H
+
+#include <WPILib.h>
+#include "Constants.h"
+#include "NumberPID.h"
+
+class Vision {
+	private:
+
+	public:
+		Vision();
+		void update();
+
+};
+
+#endif /* VISION_H */
